7207e6e62a7adf1c91fefbecbe979613724ef71d
Mikedwin <1mikedwin@gmail.com>
2026-01-19 11:48:12 +0000

    Fix CORS and FRONTEND_URL for payment to work

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ app.use(express.json({ limit: '5gb' }));
 app.use(express.urlencoded({ limit: '5gb', extended: true }));
 app.use(cors({
   origin: process.env.NODE_ENV === 'production' 
-    ? [process.env.FRONTEND_URL] 
+    ? ['https://uni-hostel-two.vercel.app', process.env.FRONTEND_URL] 
     : ['http://localhost:3000', 'http://127.0.0.1:3000'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
